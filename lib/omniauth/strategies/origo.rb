c2f77e6620d2727bb860b2048c01f645e8ab937b
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2011-11-01 14:45:43 +0100

    Clean code some

--- lib/omniauth/strategies/origo.rb
+++ lib/omniauth/strategies/origo.rb
@@ -4,45 +4,36 @@ require 'multi_json'
 module OmniAuth
   module Strategies
     class Origo < OmniAuth::Strategies::OAuth2
-      # Give your strategy a name.
+
       option :name, "origo"
 
-      # This is where you pass the options you would pass when
-      # initializing your consumer from the OAuth gem.
       option :client_options, {
         :site => 'https://secure.origo.no',
         :token_url => '/-/oauth/token',
         :authorize_url => '/-/oauth/authorize'
       }
       
-      # These are called after authentication has succeeded. If
-      # possible, you should try to set the UID without making
-      # additional calls (if the user id is returned with the token
-      # or as a URI parameter). This may not be possible with all
-      # providers.
-      uid{ user_data['result']['user']['id'].to_s }
+      uid { raw_info['result']['user']['id'].to_s } # to_s to keep all values as strings
         
-      info do
+      info do 
         {
-           'name' => user_data['result']['user']['full_name'],
-           'first_name' => user_data['result']['user']['first_name'],
-           'last_name' => user_data['result']['user']['last_name'],
-           'image' => user_data['result']['user']['image_url'],
-           'email' => user_data['result']['user']['email'],
-           'urls' => {
-             'Origo' => "http://origo.no/-/user/show/#{user_data['result']['user']['id']}"
-           }
+          'name' => raw_info['result']['user']['full_name'],
+          'first_name' => raw_info['result']['user']['first_name'],
+          'last_name' => raw_info['result']['user']['last_name'],
+          'image' => raw_info['result']['user']['image_url'],
+          'email' => raw_info['result']['user']['email'],
+          'urls' => {
+              'Origo' => "http://origo.no/-/user/show/#{raw_info['result']['user']['id']}"
+          }
         }
       end
 
       extra do
-        {
-          'raw_info' => user_data
-        }
+        { 'raw_info' => raw_info }
       end
             
-      def user_data
-        @user_data ||= access_token.get('/-/api/v2/js/user').parsed
+      def raw_info
+        @raw_info ||= access_token.get('/-/api/v2/js/user').parsed
       end
 
     end
