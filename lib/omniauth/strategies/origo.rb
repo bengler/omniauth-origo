c25eacd304a6efd1e9b5ba4c580630f10fd3183b
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2011-11-24 17:36:18 +0100

    Update Origo strategy

--- lib/omniauth/strategies/origo.rb
+++ lib/omniauth/strategies/origo.rb
@@ -4,6 +4,8 @@ require 'multi_json'
 module OmniAuth
   module Strategies
     class Origo < OmniAuth::Strategies::OAuth2
+      
+      USERINFO_ENDPOINT = 'http://origo.no/-/api/v2/user/omniauth_hash'
 
       option :name, "origo"
 
@@ -15,27 +17,18 @@ module OmniAuth
         :authorize_url => '/-/oauth/authorize'
       }
       
-      uid { raw_info['result']['user']['id'].to_s } # to_s to keep all values as strings
+      uid { raw_info['uid'].to_s } # to_s to keep all values as strings
         
       info do 
-        {
-          'name' => raw_info['result']['user']['full_name'],
-          'first_name' => raw_info['result']['user']['first_name'],
-          'last_name' => raw_info['result']['user']['last_name'],
-          'image' => raw_info['result']['user']['image_url'],
-          'email' => raw_info['result']['user']['email'],
-          'urls' => {
-              'Origo' => "http://origo.no/-/user/show/#{raw_info['result']['user']['id']}"
-          }
-        }
+        raw_info['info']
       end
 
       extra do
-        { 'raw_info' => raw_info }
+        { 'extra' => raw_info['extra'], 'raw_info' => raw_info }
       end
             
       def raw_info
-        @raw_info ||= access_token.get('/-/api/v2/js/user').parsed
+        @raw_info ||= access_token.get(USERINFO_ENDPOINT).parsed
       end
 
     end
