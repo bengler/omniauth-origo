aeea879ee9514294c4ffd52cc4beae8e27c1c14a
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2011-11-25 14:12:44 +0100

    Update spec

--- spec/omniauth/strategies/origo_spec.rb
+++ spec/omniauth/strategies/origo_spec.rb
@@ -9,28 +9,21 @@ describe OmniAuth::Strategies::Origo do
 
   describe '#client' do
     it 'has correct site URL' do
-      subject.client.site.should == 'http://secure.origo.no'
+      subject.client.site.should == 'https://secure.origo.no'
     end
 
     it 'has correct authorize URL' do
-      subject.client.authorize_url.should == 'http://secure.origo.no/-/oauth/authorize'
+      subject.client.authorize_url.should == 'https://secure.origo.no/-/oauth/authorize'
     end
 
     it 'has correct token URL' do
-      subject.client.token_url.should == 'http://secure.origo.no/-/oauth/token'
+      subject.client.token_url.should == 'https://secure.origo.no/-/oauth/token'
     end 
   end
 
   describe '#callback_path' do
     it "has the correct callback path" do
       subject.callback_path.should eq('/auth/origo/callback')
-    end
-  end
-
-  describe '#raw_data' do
-    it "connects to user info endpoint, but is forbidden without a proper auth" do
-      response = Net::HTTP.get_response URI.parse(OmniAuth::Strategies::Origo::USERINFO_ENDPOINT)
-      response.body.should == "forbidden"
     end
   end
 
